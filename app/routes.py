914860bdbf39373d7a01692636ab25bf2d3dea15
divij-sinha <80167639+divij-sinha@users.noreply.github.com>
2024-01-13 13:22:44 -0600

    start page

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,4 @@ from flask import render_template, redirect, url_for, session, request
 
 @app.route("/", methods=["GET", "POST"])
 def start_page():
-    return render_template("_base_page.html")
+    return render_template("start_page.html")
